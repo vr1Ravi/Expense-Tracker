d3d7e4a99a07298559c198db850ea5cfb55c9b27
vr1Ravi <ravikumarjha059@gmail.com>
2024-04-17 21:01:13 +0530

    cors added

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://expense-tracker-gamma-ashy.vercel.app/",
+      "https://expense-tracker-gamma-ashy.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
